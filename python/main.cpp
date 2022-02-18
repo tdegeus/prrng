ad962a22a1b276e10778a62bbca9687edfe5c074
Tom de Geus <tom@geus.me>
2022-02-18 17:01:14 +0100

    Improving Python module name

--- python/main.cpp
+++ python/main.cpp
@@ -10,8 +10,33 @@
 
 namespace py = pybind11;
 
+/**
+Overrides the `__name__` of a module.
+Classes defined by pybind11 use the `__name__` of the module as of the time they are defined,
+which affects the `__repr__` of the class type objects.
+*/
+class ScopedModuleNameOverride {
+public:
+    explicit ScopedModuleNameOverride(py::module m, std::string name) : module_(std::move(m))
+    {
+        original_name_ = module_.attr("__name__");
+        module_.attr("__name__") = name;
+    }
+    ~ScopedModuleNameOverride()
+    {
+        module_.attr("__name__") = original_name_;
+    }
+
+private:
+    py::module module_;
+    py::object original_name_;
+};
+
 PYBIND11_MODULE(_prrng, m)
 {
+    // Ensure members to display as `prrng.X` rather than `prrng._prrng.X`
+    ScopedModuleNameOverride name_override(m, "prrng");
+
     xt::import_numpy();
 
     m.doc() = "Portable Reconstructible Random Number Generator";
