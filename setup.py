7dcd53824f4308655744470fcc95c1ff517f0884
Tom de Geus <tom@geus.me>
2023-05-05 17:18:44 +0200

    Updating setup

--- setup.py
+++ setup.py
@@ -1,17 +1,11 @@
-from pathlib import Path
-
 from setuptools_scm import get_version
 from skbuild import setup
 
 project_name = "prrng"
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
 setup(
     name=project_name,
     description="Portable Reconstructible Random Number Generator",
-    long_description=long_description,
     version=get_version(),
     license="MIT",
     author="Tom de Geus",
@@ -21,4 +15,5 @@ setup(
     package_dir={"": "python"},
     cmake_install_dir=f"python/{project_name}",
     cmake_minimum_required_version="3.13",
+    python_requires=">=3.6",
 )
